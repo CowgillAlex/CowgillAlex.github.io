eda9b1f4b980b595d9f450ffe79056639ba3c9ca
19alexandercowgill <19alexandercowgill@st-ambrosecollege.org.uk>
2025-03-20 22:49:48 +0000

    update contact, update PM project, use components for head/foot

---
+++ navigation.js
@@ -0,0 +1,24 @@
+function injectLinks(){
+    const nav = document.getElementById("nav-head")
+   
+    nav.innerHTML = `
+     <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/projects/scratch/minecraft/">Projects</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="https://www.youtube.com/@AlexC_MC">YouTube</a></li>
+        <li><a href="https://edu.alexco.dev/">A Level</a></li>
+        <li><a href="#">Posts</a></li>
+        <li><a href="/IS/">IS</a></li>
+
+      </ul>
+    `
+}
+function injectFooter(){
+    const foot = document.getElementById("footer-foot");
+    foot.innerHTML = `
+          &copy; 2025 Alexander Cowgill<br>This site is under construction. Your page probably isn't there. Sorry!
+    `
+}
+injectLinks();
+injectFooter();
